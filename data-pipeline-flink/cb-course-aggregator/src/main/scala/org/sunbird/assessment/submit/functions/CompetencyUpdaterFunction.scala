d8c6170824ee841bad566787442b92f8cbfef079
sreeragksgh <sreeragsajeesh@gmail.com>
2023-01-31 17:46:54 +0530

    Removing logs and intending code

--- data-pipeline-flink/cb-course-aggregator/src/main/scala/org/sunbird/assessment/submit/functions/CompetencyUpdaterFunction.scala
+++ data-pipeline-flink/cb-course-aggregator/src/main/scala/org/sunbird/assessment/submit/functions/CompetencyUpdaterFunction.scala
@@ -21,15 +21,14 @@ import java.util
 import java.util.Map
 import scala.collection.convert.ImplicitConversions.`map AsJavaMap`
 
-class competencyUpdaterFunction(config: AssessmentConfig,
+class CompetencyUpdaterFunction(config: AssessmentConfig,
                                 @transient var cassandraUtil: CassandraUtil = null
-                      )(implicit val mapTypeInfo: TypeInformation[Event])
+                               )(implicit val mapTypeInfo: TypeInformation[Event])
   extends BaseProcessFunction[Event, Event](config) {
 
-  private[this] val logger = LoggerFactory.getLogger(classOf[competencyUpdaterFunction])
+  private[this] val logger = LoggerFactory.getLogger(classOf[CompetencyUpdaterFunction])
   private var restUtil: RestUtil = _
   private var restApiUtil: RestApiUtil = _
-
 
   override def metricsList() = List(config.updateCount, config.failedEventCount)
 
@@ -54,64 +53,48 @@ class competencyUpdaterFunction(config: AssessmentConfig,
                               context: ProcessFunction[Event, Event]#Context,
                               metrics: Metrics): Unit = {
     try {
-      logger.info("Entering PassbookFunction " + event.toString)
       if (event.actor.get("id").equalsIgnoreCase(config.CERTIFICATE_GENERATOR)) {
         val userId = event.edata.get(config.USER_ID)
-        logger.info ("user id of this user is :"+userId)
         val related = event.edata.get(config.RELATED).asInstanceOf[util.HashMap[String, Any]]
         val courseId = related.get(config.COURSE_ID)
         val courseCompetencies = enrichCompetencyV3(courseId).asInstanceOf[util.List[util.Map[String, Any]]]
-        logger.info("Course competencies new"+ courseCompetencies)
         val profileDetails = getUserProfileDetails(userId).asInstanceOf[util.Map[String, Any]]
-        logger.info("before competency")
         var k: Int = 0
 
         if (profileDetails.containsKey(config.competencies)) {
           k = 1
           var userCompetencies: util.List[Map[String, Any]] = profileDetails.getOrDefault(config.competencies, null).asInstanceOf[util.List[Map[String, Any]]]
-          logger.info("after competency" + userCompetencies)
           val userCompetenciesMapJava = new java.util.HashMap[String, Object]()
 
           userCompetencies.forEach(content => {
-            logger.info("inside loop userCompetencies.forEach" + content.toString)
             userCompetenciesMapJava.put(content.get(config.id).asInstanceOf[String], content)
-            logger.info("inside user competency loop")
           })
 
           val userCompetenciesMap: Map[String, Any] = userCompetenciesMapJava.asInstanceOf[util.Map[String, Any]]
 
           courseCompetencies.forEach(courseContent => {
-            logger.info("inside course competency loop int")
             var courseCID: String = courseContent.get(config.id).asInstanceOf[String]
             var courseLevelValue: String = courseContent.get(config.selectedLevelLevel).asInstanceOf[String]
             var courseLevelName: String = courseContent.get(config.selectedLevelName).asInstanceOf[String]
             val competencyLevel = courseLevelValue.split(" ")(1)
             val competencyIntLevel = competencyLevel.toInt
-            logger.info("int error resolved new" + "   " + competencyLevel + "   " + competencyIntLevel)
             if (userCompetenciesMap.containsKey(courseCID)) {
-              logger.info("inside user course loop")
-              var userContent: Map[String, Any] = userCompetenciesMap.get(courseCID).asInstanceOf[Map[String, Any]]
-              var userLevelValue: String = userContent.get(config.competencyCBPCompletionLevel).asInstanceOf[String]
-              logger.info("user level" + userLevelValue)
+              var userCompetency: Map[String, Any] = userCompetenciesMap.get(courseCID).asInstanceOf[Map[String, Any]]
+              var userLevelValue: String = userCompetency.get(config.competencyCBPCompletionLevel).asInstanceOf[String]
               var userLevel: Int = 0
               if (null != userLevelValue) {
-                logger.info("inside level condition")
                 var userLevel: Int = userLevelValue.toInt
-                logger.info("competencylevel " + competencyIntLevel + "    " + "userlevel" + userLevel)
-
                 if (null != competencyIntLevel) {
                   if (competencyIntLevel > userLevel) {
-                    logger.info("inside competency user level check")
-                    userContent.put(config.competencyCBPCompletionLevel, competencyLevel)
-                    userContent.put(config.competencyCBPCompletionLevelValue, courseLevelValue)
-                    userContent.put(config.competencyCBPCompletionLevelName, courseLevelName)
+                    userCompetency.put(config.competencyCBPCompletionLevel, competencyLevel)
+                    userCompetency.put(config.competencyCBPCompletionLevelValue, courseLevelValue)
+                    userCompetency.put(config.competencyCBPCompletionLevelName, courseLevelName)
                   }
                 }
               } else {
-                logger.info("inside else part")
-                userContent.put(config.competencyCBPCompletionLevel, competencyLevel)
-                userContent.put(config.competencyCBPCompletionLevelValue, courseLevelValue)
-                userContent.put(config.competencyCBPCompletionLevelName, courseLevelName)
+                userCompetency.put(config.competencyCBPCompletionLevel, competencyLevel)
+                userCompetency.put(config.competencyCBPCompletionLevelValue, courseLevelValue)
+                userCompetency.put(config.competencyCBPCompletionLevelName, courseLevelName)
               }
             } else {
               var newCompetencyMap: Map[String, Any] = new util.HashMap()
@@ -125,20 +108,16 @@ class competencyUpdaterFunction(config: AssessmentConfig,
               newCompetencyMap.put(config.competencyCBPCompletionLevelName, courseLevelName)
               newCompetencyMap.put(config.osid, java.util.UUID.randomUUID.toString)
               userCompetencies.add(newCompetencyMap)
-              logger.info("New Competency Map With Added Competency old ${userCompetencies} :")
-              logger.info("Newly Updated Profile Details ${profileDetails} :")
             }
             profileDetails.put(config.competencies, userCompetencies)
           })
         } else {
           var userCompetenciesList: util.List[Map[String, Any]] = new util.ArrayList[Map[String, Any]]()
           courseCompetencies.forEach(courseContent => {
-            logger.info("inside course competency loop int else part")
             var courseCID: String = courseContent.get(config.id).asInstanceOf[String]
             var courseLevelValue: String = courseContent.get(config.selectedLevelLevel).asInstanceOf[String]
             var courseLevelName: String = courseContent.get(config.selectedLevelName).asInstanceOf[String]
             val competencyLevel = courseLevelValue.split(" ")(1)
-
             var newCompetencyMap: Map[String, Any] = new util.HashMap()
             newCompetencyMap.put(config.id, courseCID)
             newCompetencyMap.put(config.name, courseContent.get(config.name).asInstanceOf[String])
@@ -149,14 +128,10 @@ class competencyUpdaterFunction(config: AssessmentConfig,
             newCompetencyMap.put(config.competencyCBPCompletionLevelValue, courseLevelValue)
             newCompetencyMap.put(config.competencyCBPCompletionLevelName, courseLevelName)
             newCompetencyMap.put(config.osid, java.util.UUID.randomUUID.toString)
-            logger.info("new Course competency map added : " + newCompetencyMap)
-            logger.info("before addition of new competency userCompetenciesList size : " + userCompetenciesList.size())
             userCompetenciesList.add(newCompetencyMap)
-            logger.info("after addition of new competency userCompetenciesList size : " + userCompetenciesList.size())
           })
           profileDetails.put(config.competencies, userCompetenciesList)
         }
-        logger.info("profile details" + profileDetails)
         updateProfileDetails(profileDetails, userId)
       }
     } catch {
@@ -180,8 +155,8 @@ class competencyUpdaterFunction(config: AssessmentConfig,
       filters.put(config.PRIMARY_CATEGORY, new util.ArrayList[String]() {
         add("Course")
       })
-      val sortBy=new util.HashMap[String,Any]()
-      sortBy.put(config.lastUpdatedOn,config.DESC)
+      val sortBy = new util.HashMap[String, Any]()
+      sortBy.put(config.lastUpdatedOn, config.DESC)
       val request = new util.HashMap[String, Any]()
       request.put(config.FILTERS, filters)
       request.put(config.SORT_BY, sortBy)
@@ -201,7 +176,6 @@ class competencyUpdaterFunction(config: AssessmentConfig,
           })
         }
       }
-      logger.info("competency v3: "+competencyV3)
       competencyV3
     }
     catch {
@@ -211,21 +185,18 @@ class competencyUpdaterFunction(config: AssessmentConfig,
     }
   }
 
-  def getUserProfileDetails(userId: String): util.Map[String, Any]  = {
+  def getUserProfileDetails(userId: String): util.Map[String, Any] = {
     if (!userId.isEmpty) {
       val query = QueryBuilder.select(config.profileDetails)
         .from(config.dbSunbirdKeyspace, config.userTable)
         .where(QueryBuilder.eq(config.userId, userId)).allowFiltering().toString
       val record = cassandraUtil.findOne(query)
       val profileDetails = record.getString(config.profileDetails)
-      logger.info(s"profileDetails${profileDetails} :")
       if (!profileDetails.isEmpty || profileDetails != null) {
         val mapper = new ObjectMapper
         val profileDetailsMapper = mapper.readValue(profileDetails, classOf[Map[String, Any]])
-        logger.info(s"profileDetailsMapper: ${profileDetailsMapper} :")
         profileDetailsMapper
       } else {
-        logger.info(s"competencies: ${"competencies"} :")
         return null
       }
     } else {
@@ -233,19 +204,17 @@ class competencyUpdaterFunction(config: AssessmentConfig,
     }
   }
 
-  def updateProfileDetails(profileDetails:  util.Map[String, Any], userId: String): Unit = {
+  def updateProfileDetails(profileDetails: util.Map[String, Any], userId: String): Unit = {
     try {
       val mapper = new ObjectMapper
-      logger.info("inside update method"+"   "+"userid "+userId)
       val query = QueryBuilder.update(config.dbSunbirdKeyspace, config.userTable)
         .`with`(QueryBuilder.set(config.profileDetails, mapper.writeValueAsString(profileDetails)))
         .where(QueryBuilder.eq(config.id, userId))
-      logger.info(s"Query: ${query}")
       cassandraUtil.upsert(query.toString)
     } catch {
       case exception: Exception =>
         exception.printStackTrace()
-        logger.info("Updating Profile Details Failed :"+exception.getMessage())
+        logger.info("Updating Profile Details Failed :" + exception.getMessage())
     }
   }
 }
