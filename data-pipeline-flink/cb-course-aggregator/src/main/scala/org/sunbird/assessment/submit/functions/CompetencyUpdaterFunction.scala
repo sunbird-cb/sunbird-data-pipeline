a175957fdc4a95849e0eb80e612cd09413556bbd
karthik-tarento <karthikeyan.rajendran@tarento.com>
2023-02-03 09:10:36 +0530

    Removed debug logs and added info log

--- data-pipeline-flink/cb-course-aggregator/src/main/scala/org/sunbird/assessment/submit/functions/CompetencyUpdaterFunction.scala
+++ data-pipeline-flink/cb-course-aggregator/src/main/scala/org/sunbird/assessment/submit/functions/CompetencyUpdaterFunction.scala
@@ -53,7 +53,6 @@ class CompetencyUpdaterFunction(config: AssessmentConfig,
     logger.info("Received Event in CompetencyUpdaterFunction. Event: " + event.toString)
     try {
       if (event.actor.get("id").equalsIgnoreCase(config.CERTIFICATE_GENERATOR)) {
-        logger.info("Processing course cert issue event. ")
         val userIds = event.edata.get(config.USER_IDS).asInstanceOf[util.List[String]]
         //TODO -- need to loop through the list of received userIds
         val userId = userIds.get(0)
@@ -109,6 +108,7 @@ class CompetencyUpdaterFunction(config: AssessmentConfig,
           profileDetails.put(config.competencies, userCompetenciesList)
         }
         updateProfileDetails(profileDetails, userId)
+        logger.info("Successfully updated the user profile with competencies.")
       } else {
         logger.info("Ignoring event as it is not configured to process it.")
       }
